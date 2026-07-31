7429c92f8bdcff67c9c0a2b21f58271a88161b00
PREMANSHU05 <premanshupadwal@gmail.com>
2026-07-31 18:31:55 +0530

    Prepared backend server for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
 require("dotenv").config();
+
 const app = require("./app");
-const { connect, connection } = require("mongoose");
 const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoutes");
+
 const PORT = process.env.PORT || 5000;
 
-const startserver = async () => {
+const startServer = async () => {
   try {
     await connectDB();
+
     app.listen(PORT, () => {
-      console.log(`server running on port ${PORT}`);
+      console.log(`🚀 Server running on port ${PORT}`);
     });
   } catch (error) {
-    console.log(error.message);
+    console.log("Server Error:", error.message);
+    process.exit(1);
   }
 };
-startserver();
+
+startServer();
